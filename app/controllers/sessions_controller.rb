6268228678fad746ef49f71688a689bfe40ad751
Vladyslav <vkadyrov@protonmail.com>
2023-07-19 17:45:09 -0400

    Added basic SessionsController & Private method for ApplicationController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  # login
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
+    end
+  end
+
+  # logout
+
+  def destroy
+    session.clear
+    render json: { message: 'Logged out successfully' }, status: :ok
+  end
+end
