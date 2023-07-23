0c152f621b929866342ebedfe404afdbbc693f99
Vladyslav <vkadyrov@protonmail.com>
2023-07-23 13:01:39 -0400

    Working on the backend

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,15 +1,31 @@
 class AppointmentsController < ApplicationController
   def index
+    appointments = Appointment.all
+    redner json: appointments
   end
 
   def create
+    appointment = current_user.appointment.create(appointment_params)
+    if appointment.valid?
+      render json: appointment, status: :created
+    else
+      render json: {
+               errors: appointment.errors.full_messages,
+             },
+             status: :unprocessable_entity
+    end
   end
 
   def update
+    # appointment = current_user
   end
 
   def destroy
   end
 
   private
+
+  def appointment_params
+    params.require(:appointment).permit(:patient_id, :name, :date_time, :reason)
+  end
 end
