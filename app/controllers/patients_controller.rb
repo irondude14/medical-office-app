fc03d044e410a078a0991f4cb951321521dbd6e1
Vladyslav <vkadyrov@protonmail.com>
2023-07-25 19:32:51 -0400

    All routes for the ParientsController

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,16 +2,49 @@ class PatientsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    patients = Patient.all
+    redner json: patients
+  end
+
+  def show
+    render json: @patient
   end
 
   def create
+    patient = Patient.create(patient_params)
+    if patient.valid?
+      render json: patient, status: :created
+    else
+      render json: {
+               errors: patient.errors.full_messages,
+             },
+             status: :unprocessable_entity
+    end
   end
 
   def update
+    if @patient.update(patient_update_params)
+      render json: @patient
+    else
+      render json: {
+               errors: @patient.errors.full_messages,
+             },
+             status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @patient.destroy
+    render json: { message: 'Patient deleted' }, status: :ok
   end
 
   private
+
+  def patient_params
+    params.require(:patient).permit(:patient_id, :user_id, :date_time, :reason)
+  end
+
+  def patient_update_params
+    params.require(:patient).permit(:user_id, :date_time, :reason)
+  end
 end
