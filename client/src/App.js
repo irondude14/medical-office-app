66147297047e7df47459da8baff563f4c10460cd
Vladyslav <vkadyrov@protonmail.com>
2023-07-31 18:57:49 -0400

    Changes to the App.js and Navbar componenet

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 
 function App() {
-  return <div>You're on React</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Navbar />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        <Route path='login' element={<Login />} />
+        <Route path='profile' element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
