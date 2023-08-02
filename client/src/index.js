1e3025e5adbd64e89ee8d52784fe477ae71a6737
Vladyslav <vkadyrov@protonmail.com>
2023-08-02 19:14:59 -0400

    Setting up Redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {},
+});
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
