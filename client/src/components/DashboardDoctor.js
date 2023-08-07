32bb06c87c0297a5d4d28f58f8816e4ddf65a2e8
Vladyslav <vkadyrov@protonmail.com>
2023-08-07 19:44:04 -0400

    git commit -m

--- client/src/components/DashboardDoctor.js
+++ client/src/components/DashboardDoctor.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function DashboardDoctor() {
-  return <div>This will be a dashboard for a Doctor.</div>;
+  const user = useSelector((state) => state.user.value);
+  const navigate = useNavigate();
+
+  const patientList =
+    user.patients &&
+    user.patients.map((patient) => (
+      <div key={patient.id}>
+        <h4>Patient Name: {patient.name}</h4>
+        <p>Phone number: {patient.phone}</p>
+        <p>Email: {patient.email}</p>
+
+        <h4>Upcoming appointments:</h4>
+        {user.appointments &&
+        user.appointments.filter(
+          (appointment) => appointment.patient_id === patient.id
+        ).length > 0 ? (
+          user.appointments
+            .filter((appointment) => appointment.patient_id === patient.id)
+            .map((appointment) => {
+              const appointmentDate = new Date(appointment.date_time);
+              const formattedDate = appointmentDate.toLocaleDateString();
+              const formattedTime = appointmentDate.toLocaleTimeString();
+              return (
+                <div key={appointment.id} className='appointment-card'>
+                  <p>Appointment Date: {formattedDate}</p>
+                  <p>Appointment Time: {formattedTime}</p>
+                </div>
+              );
+            })
+        ) : (
+          <p>No upcoming appointments.</p>
+        )}
+      </div>
+    ));
+
+  console.group(user);
+  return (
+    <>
+      <div>
+        <h3>Patients:</h3>
+        {patientList}
+      </div>
+    </>
+  );
 }
 
 export default DashboardDoctor;
