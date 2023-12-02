400dc385c4e50d4338f86488bdf801fca08823f8
Vladyslav <vkadyrov@protonmail.com>
2023-12-02 16:31:38 -0500

    Working on the calendar features

--- client/src/components/DashboardCalendar.js
+++ client/src/components/DashboardCalendar.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 // import { useSelector } from 'react-redux';
 
 function DashboardCalendar({ appointments }) {
   // const appointments = useSelector((state) => state.appointments.value);
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (nextDate) => {
+    setDate(nextDate);
+  };
+
+  const appointmentCheck = (date) => {
+    return appointments.some((appointment) => {
+      const appointmentDate = new Date(appointment.date_time);
+      return appointmentDate.toDateString() === date.toDateString();
+    });
+  };
+
+  const tileClassName = ({ date, view }) => {
+    // Add a class to dates with appointments
+    if (view === 'month' && appointmentCheck(date)) {
+      return 'highlight';
+    }
+  };
 
   console.log(appointments);
 
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <Calendar
+        onChange={onChange}
+        value={date}
+        tileClassName={tileClassName}
+      />
+    </div>
+  );
 }
 
-export default Calendar;
+export default DashboardCalendar;
