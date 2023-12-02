2854b170a8a47c8449d2ffa50eaf4838916750f8
Vladyslav <vkadyrov@protonmail.com>
2023-12-02 14:07:52 -0500

    Adding dedicated calendar component

---
+++ client/src/components/Calendar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function Calendar() {
+  return <div>Calendar</div>;
+}
+
+export default Calendar;
